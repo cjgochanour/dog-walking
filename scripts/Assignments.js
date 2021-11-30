2c2f22a46a67b171975e9eb416089cc73c96ad36
cjgochanour <cjgochanour@gmail.com>
2021-11-30 17:48:40 -0600

    Assignments.js fixed

--- scripts/Assignments.js
+++ scripts/Assignments.js
@@ -1,8 +1,9 @@
-import { getPets, getWalkers } from "./database.js";
+import { getPets, getWalkers, getCities } from "./database.js";
 
 // Get copy of state for use in this module
 const pets = getPets();
 const walkers = getWalkers();
+const cities = getCities();
 
 // Function whose responsibility is to find the walker assigned to a pet
 const findWalker = (pet, allWalkers) => {
@@ -23,10 +24,16 @@ export const Assignments = () => {
 
     for (const currentPet of pets) {
         const currentPetWalker = findWalker(currentPet, walkers);
+        let parsedCity;
+        for (const city of cities) {
+            if (city.id === currentPetWalker.cityId) {
+                parsedCity = city;
+            }
+        }
         assignmentHTML += `
             <li>
                 ${currentPet.name} is being walked by
-                ${currentPetWalker.name} in ${currentPetWalker.city}
+                ${currentPetWalker.name} in ${parsedCity.name}
             </li>
         `;
     }
--- scripts/CityList.js
+++ scripts/CityList.js
@@ -1,6 +1,5 @@
-import { getWalkers, getCities } from "./database.js";
+import { getCities } from "./database.js";
 
-const walkers = getWalkers();
 const cities = getCities();
 
 export const CityList = () => {
