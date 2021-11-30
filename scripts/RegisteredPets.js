5343f0a8b024e0c6a2c5901f21470da19b1851e0
cjgochanour <cjgochanour@gmail.com>
2021-11-30 16:32:59 -0600

    added window alert for pets section

--- scripts/RegisteredPets.js
+++ scripts/RegisteredPets.js
@@ -1,16 +1,32 @@
-import { getPets } from "./database.js"
+import { getPets, getWalkers } from "./database.js";
 
-const pets = getPets()
+const pets = getPets();
+const walkers = getWalkers();
 
 export const RegisteredPets = () => {
-    let petHTML = "<ul>"
+    let petHTML = "<ul>";
 
     for (const pet of pets) {
-        petHTML += `<li>${pet.name}</li>`
+        petHTML += `<li id="pet--${pet.id}">${pet.name}</li>`;
     }
 
-    petHTML += "</ul>"
+    petHTML += "</ul>";
 
-    return petHTML
-}
+    return petHTML;
+};
 
+document.addEventListener("click", (event) => {
+    const petClicked = event.target;
+    if (petClicked.id.startsWith("pet")) {
+        const [, petId] = petClicked.id.split("--");
+
+        for (const walker of walkers) {
+            if (parseInt(petId) === walker.id) {
+                let thePet = pets.find((pet) => pet.id === parseInt(petId));
+                window.alert(
+                    `${thePet.name} is being walked by ${walker.name}`
+                );
+            }
+        }
+    }
+});
